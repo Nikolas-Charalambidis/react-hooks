8fd2d65ccdbd62d17ed334031c2a0bac0bb2516f
Nikolas Charalambidis <chan01@vse.cz>
2019-09-28 16:37:03 +0200

    Updates

---
+++ precache-manifest.6de6d24b2c18d880d46d64457f71360e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f925c9d837c2f35a033e07f1caaa906",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fa002cd208d8a0c37d38",
+    "url": "./static/css/main.80db8e04.chunk.css"
+  },
+  {
+    "revision": "e285339d80e79f6668eb",
+    "url": "./static/js/2.6692b981.chunk.js"
+  },
+  {
+    "revision": "fa002cd208d8a0c37d38",
+    "url": "./static/js/main.72c0dc11.chunk.js"
+  },
+  {
+    "revision": "538125f763a90ae260cf",
+    "url": "./static/js/runtime-main.24afb4bd.js"
+  }
+]);
